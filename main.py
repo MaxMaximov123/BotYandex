15f75e9997da53b6ae74bd3f85ba45817bd63d37
MaxMaximov123 <99262907+MaxMaximov123@users.noreply.github.com>
2023-04-06 09:51:00 +0300

    Update main.py
    .

--- main.py
+++ main.py
@@ -485,7 +485,7 @@ async def main():
 	Thread(target=save_all).start()
 
 	every(10).minutes.do(save_all)
-	every().day.at("06:25").do(lambda: asyncio.run(start_mailing()))
+	every().day.at("06:25").do(asyncio.run, start_mailing())
 	await dp.start_polling(bot)
 
 
